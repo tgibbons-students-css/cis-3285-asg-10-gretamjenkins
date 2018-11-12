279fda3869faffac56cd958e23f5d716eb9fd900
Greta Jenkins <gjenkins@css.edu>
2018-11-12 16:41:03 -0600

    Added comments

--- CurrencyTrader.AdoNet/AsyncTradeStorage.cs
+++ CurrencyTrader.AdoNet/AsyncTradeStorage.cs
@@ -9,16 +9,24 @@ namespace CurrencyTrader.AdoNet
 {
     public class AsyncTradeStorage : ITradeStorage
     {
+        // Local variables to store information and facilitate trade storage process
         private readonly ILogger logger;
         private ITradeStorage syncTradeStorage;
 
+        /// <summary>
+        /// Constructor for async storage. Creates a new logger and synchronous trade object
+        /// </summary>
+        /// <param name="logger"></param>
         public AsyncTradeStorage(ILogger logger)
         {
             this.logger = logger;
             syncTradeStorage = new AdoNetTradeStorage(logger);
-
         }
 
+        /// <summary>
+        /// Persists the trade data through to the sync object through a separate, asynchronous process
+        /// </summary>
+        /// <param name="trades"></param>
         public void Persist(IEnumerable<TradeRecord> trades)
         {
             logger.LogInfo("Starting synchronous trade storage");
