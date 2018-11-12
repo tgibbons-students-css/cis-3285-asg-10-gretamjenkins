1689444695109da6b36d05a6cfbfbd1daf3023ae
Greta Jenkins <gjenkins@css.edu>
2018-11-12 16:37:00 -0600

    Created a new async decorator class and implemented it. No more delays in storing trades

---
+++ CurrencyTrader.AdoNet/AsyncTradeStorage.cs
@@ -0,0 +1,28 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+    public class AsyncTradeStorage : ITradeStorage
+    {
+        private readonly ILogger logger;
+        private ITradeStorage syncTradeStorage;
+
+        public AsyncTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+            syncTradeStorage = new AdoNetTradeStorage(logger);
+
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            logger.LogInfo("Starting synchronous trade storage");
+            Task.Run(() => syncTradeStorage.Persist(trades));
+        }
+    }
+}
